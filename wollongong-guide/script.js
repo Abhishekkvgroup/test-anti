ce3b458e294fcea5672fdf85bd1970c728d19f12
Abhishekkvgroup <Abhishek@kvgroupglobal.com>
2026-03-11 08:17:17 +0530

    Permanently remove redundant project folders from git tracking

--- wollongong-guide/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('start-btn').addEventListener('click', () => {
-    document.querySelectorAll('.slide')[0].classList.remove('active');
-    document.querySelectorAll('.slide')[1].classList.add('active');
-});
--- wollongong-guide/strip_images.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let html = fs.readFileSync('c:/Users/KVGROUP-189/Desktop/test/wollongong-guide/content.html', 'utf8');
-// remove img tags with base64
-html = html.replace(/<img[^>]+src="data:image[^>]+>/g, '');
-fs.writeFileSync('c:/Users/KVGROUP-189/Desktop/test/wollongong-guide/clean_content.html', html);
